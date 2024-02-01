d5e0220ddc6ae50d253c2167cfde0a2752ce9bfc
Mahan Bayhaghi <mahan.bayhaghi@gmail.com>
2024-02-01 21:55:33 +0330

    read all 5 batches

--- super_test.c
+++ super_test.c
@@ -12,18 +12,12 @@
 
 #define NUM_CLASSES 10
 #define NUM_IMAGES_PER_FILE 10000
+#define NUM_BATCHES 5
 #define IMAGE_DIMENSION 32
 
 #define NUM_PROCESSES 1
 
 #define MAX 10 
-
-struct mseg_buffer
-{
-    long mesg_type;
-    char mesg_text[100];
-} message;
-
 
 typedef struct {
     unsigned char r, g, b;
@@ -52,105 +46,114 @@ void read_cifar10(char *filename, cifar10 *data) {
 }
 
 void child_process(cifar10* data , int* stoppage , int classes){
-	int class1 = classes*2;
-	int class2 = class1 + 1;
-    	printf("I'm child %d and I have access to data\n", getpid());
+    int class1 = classes*2;
+    int class2 = class1 + 1;
+    printf("I'm child %d and I have access to data\n", getpid());
 
-	// TODO: this process should only access datas with lable class1 or class2. simply implement this by iterating on data and extracting
     char weight_send_fifo[20];
-    sprintf(weight_send_fifo, "/tmp/wsfifo%d",classes);
-    mkfifo(weight_send_fifo,0777);
-    printf("weight_send_fifo is made");
+    sprintf(weight_send_fifo, "/tmp/wsfifo%d", classes);
+    mkfifo(weight_send_fifo, 0777);
+    printf("weight_send_fifo is made\n");
 
-    char weight_recieve_fifo[20];
-    sprintf(weight_recieve_fifo, "/tmp/wrfifo%d",classes);
-    mkfifo(weight_recieve_fifo,0777);
-    printf("weight_recieve_fifo is made");
+    char weight_receive_fifo[20];
+    sprintf(weight_receive_fifo, "/tmp/wrfifo%d", classes);
+    mkfifo(weight_receive_fifo, 0777);
+    printf("weight_receive_fifo is made\n");
 
     char image_send_fifo[20];
-    sprintf(image_send_fifo, "/tmp/isfifo%d",classes);
-    mkfifo(image_send_fifo,0777);
-    printf("image_send_fifo is made");
-
+    sprintf(image_send_fifo, "/tmp/isfifo%d", classes);
+    mkfifo(image_send_fifo, 0777);
+    printf("image_send_fifo is made\n");
 
     int fd1 = open(weight_send_fifo, O_WRONLY);
-    if (fd == -1) {
+    if (fd1 == -1) {
         perror("Error opening the FIFO for writing weight");
         exit(EXIT_FAILURE);
     }
     // TODO: send weights
 
     int fd2 = open(image_send_fifo, O_WRONLY);
-    if (fd == -1){
-	perror("Error opening the FIFO for writing images");
+    if (fd2 == -1) {
+        perror("Error opening the FIFO for writing images");
         exit(EXIT_FAILURE);
     }
-    // Send a sample CIFAR-10 instance to the Python script
-    //write(fd, data[0].image, sizeof(pixel) * IMAGE_DIMENSION * IMAGE_DIMENSION);
-    //write(fd, &(data[0].label), sizeof(unsigned char));
+    // Send CIFAR-10 instances to the Python script
+    write(fd2, data, sizeof(cifar10) * NUM_IMAGES_PER_FILE);
+    // Close the named pipes
+    close(fd1);
+    close(fd2);
+
+    int fd3 = open(weight_receive_fifo, O_RDONLY);
+    if (fd3 == -1) {
+        perror("Error opening the FIFO for receiving updated weights");
+        exit(EXIT_FAILURE);
+    }
+    // TODO: receive data and aggregate weights. Also, check if we should stop or not 
+
     // Close the named pipe
-    // TODO: send single or multiple images ?
-
-    int fd3 = open(weight_recieve_fifo, O_WRONLY);
-    if (fd == -1){
-	perror("Error opening the FIFO for recieving updated weights");
-        exit(EXIT_FAILURE);
-    }
-    // TODO: recieve data and aggregate weights. also check if we should stop or not 
-
-
+    close(fd3);
 }
 
 int main() {
     int shm_stoppage = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
-    if (shm_stoppage < 0){
+    if (shm_stoppage < 0) {
         perror("stoppage couldn't be shared");
         return 1;
     }
-    int shmid = shmget(IPC_PRIVATE, NUM_IMAGES_PER_FILE * sizeof(cifar10), IPC_CREAT | 0666);
+    int shmid = shmget(IPC_PRIVATE, NUM_IMAGES_PER_FILE * NUM_BATCHES * sizeof(cifar10), IPC_CREAT | 0666);
     if (shmid < 0) {
         perror("data couldn't be shared");
         return 1;
     }
-	
-    int* stoppage = shmat(shm_stoppage,NULL, 0);
-    if (stoppage == (int*) -1){
+
+    int* stoppage = shmat(shm_stoppage, NULL, 0);
+    if (stoppage == (int*) -1) {
         perror("stoppage is not shared");
         return 1;
     }
+
     cifar10 *data = shmat(shmid, NULL, 0);
     if (data == (cifar10 *) -1) {
         perror("data is not shared");
         return 1;
     }
+
     printf("initializing stoppage condition to 0\n");
     *stoppage = 0;
-    printf("starting to read batch 1...\n");
-    read_cifar10("data_batch_1.bin", data);
-    printf("batch 1 reading done\n");
-    printf("sizeof(data) = %ld kB\n", sizeof(cifar10)*NUM_IMAGES_PER_FILE / (1024));
-    
-    for (int kid=0; kid<NUM_PROCESSES; kid++){
-	    pid_t pid = fork();
-	    if (pid < 0){
-		    perror("fork failed");
-		    return 1;
-	    }
-	    if (pid==0){
-		    // child process
-		    child_process(data,stoppage,kid);
-		    exit(0);
-	    }
-        else {  // parent process
 
+    // Read all batches of CIFAR-10 data
+    for (int batch = 1; batch <= NUM_BATCHES; batch++) {
+        char filename[20];
+        sprintf(filename, "data_batch_%d.bin", batch);
+        printf("starting to read batch %d...\n", batch);
+        read_cifar10(filename, data + (batch - 1) * NUM_IMAGES_PER_FILE);
+        printf("batch %d reading done\n", batch);
+    }
+
+    printf("sizeof(data) = %ld kB\n", sizeof(cifar10) * NUM_IMAGES_PER_FILE * NUM_BATCHES / (1024));
+
+    for (int kid = 0; kid < NUM_PROCESSES; kid++) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork failed");
+            return 1;
+        }
+        if (pid == 0) {
+            // child process
+            child_process(data, stoppage, kid);
+            exit(0);
+        } else {  // parent process
+            // TODO: Additional parent process logic if needed
         }
     }
-    
+
+    // parent process
+    // TODO: Wait for all child processes to finish
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        wait(NULL);
+    }
+
+    // Cleanup shared memory
     if (shmdt(data) == -1) {
         perror("shmdt");
-        return 1;
-   	 }
-    return 0;
-   }
-
-
+        return 1
